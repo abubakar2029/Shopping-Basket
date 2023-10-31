488baf5f7bbf9aa552ebafa41cd375043af7f9f5
Abu Bakar <abubakarzafar2029@gmail.com>
2023-10-31 20:00:06 +0500

    added toastify

--- server/server.js
+++ server/server.js
@@ -598,7 +598,7 @@ app.post("/send-mail", (req, res) => {
   res.send("Done");
 });
 
-app.use(express.static('./build'))
+app.use(express.static('./build' ))
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
